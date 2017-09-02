dce120df8763afd9471336a7732bb8152b4df8e6
timabbasov <abbasovt_93@mail.ru>
2017-09-02 19:00:33 +0500

    Добавил проверку формы и наложение эффектов на фото

--- js/pictures.js
+++ js/pictures.js
@@ -5,6 +5,12 @@ var MIN_NUMBERS_LIKE = 15;
 var MAX_NUMBERS_LIKE = 200;
 var ENTER_KEYCODE = 13;
 var ESC_KEYCODE = 27;
+var MIN_SCALE = 25;
+var MAX_SCALE = 100;
+var INDEX_OF_STRING = 7;
+var HASHTAG = '#';
+var MAX_NUMBER_OF_HASHTAG = 5;
+var MAX_CHAR_OF_STRING = 20;
 var COMMENTS = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -82,12 +88,16 @@ var fillDOM = function (arr) {
 
 var onOverlayEscPress = function (evt) {
   if (evt.keyCode === ESC_KEYCODE) {
-    closeOverlay();
+    if (!(formDescription === document.activeElement)) {
+      closeOverlay();
+    }
   }
 };
 
 var closeOverlay = function () {
   galleryPhoto.classList.add('hidden');
+  uploadOverlay.classList.add('hidden');
+
   document.removeEventListener('keydown', onOverlayEscPress);
 };
 
@@ -98,8 +108,6 @@ var galleryPhotoClose = document.querySelector('.gallery-overlay-close');
 var galleryPhotoImage = document.querySelector('.gallery-overlay-image');
 
 var photos = createPhotos();
-
-document.querySelector('.upload-overlay').classList.add('hidden');
 
 fillDOM(photos);
 
@@ -130,5 +138,137 @@ galleryPhotoClose.addEventListener('click', closeOverlay);
 galleryPhotoClose.addEventListener('keydown', function (evt) {
   if (evt.keyCode === ENTER_KEYCODE) {
     closeOverlay();
+  }
+});
+
+var form = document.querySelector('#upload-select-image');
+var uploadPhoto = form.querySelector('#upload-file');
+var uploadOverlay = form.querySelector('.upload-overlay');
+var formCancel = form.querySelector('.upload-form-cancel');
+var formDescription = form.querySelector('.upload-form-description');
+var formHashtags = form.querySelector('.upload-form-hashtags');
+var formSubmit = form.querySelector('.upload-form-submit');
+var photo = form.querySelector('.effect-image-preview');
+var resizeControls = form.querySelector('.upload-resize-controls-value');
+var resizeControlDec = form.querySelector('.upload-resize-controls-button-dec');
+var resizeControlInc = form.querySelector('.upload-resize-controls-button-inc');
+var effectControls = form.querySelector('.upload-effect-controls');
+
+var checkSimilarHashtag = function (arr) {
+  var obj = {};
+
+  for (var i = 0; i < arr.length; i++) {
+    if (obj[arr[i]]) {
+      return true;
+    }
+    obj[arr[i]] = true;
+  }
+  return false;
+};
+
+var checkForSpace = function (arr) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].slice(1).search(HASHTAG) !== -1) {
+      return true;
+    }
+  }
+  return false;
+};
+
+var checkCharHashtag = function (arr) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].slice(0, 1) !== HASHTAG) {
+      return true;
+    }
+  }
+  return false;
+};
+
+var checkLengthOfString = function (arr) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].length > MAX_CHAR_OF_STRING) {
+      return true;
+    }
+  }
+  return false;
+};
+
+var checkNumberOfHashtag = function (arr) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr.length > MAX_NUMBER_OF_HASHTAG) {
+      return true;
+    }
+  }
+  return false;
+};
+
+resizeControls.value = MAX_SCALE;
+
+uploadPhoto.addEventListener('change', function () {
+  uploadOverlay.classList.remove('hidden');
+  document.addEventListener('keydown', onOverlayEscPress);
+});
+
+formCancel.addEventListener('click', function () {
+  uploadOverlay.classList.add('hidden');
+});
+
+formSubmit.addEventListener('click', function (evt) {
+  var arrHashtags = formHashtags.value.split(' ');
+  if (formHashtags.value.trim()) {
+    if (checkCharHashtag(arrHashtags)) {
+      evt.preventDefault();
+      formHashtags.classList.add('upload-message-error');
+    } else if (checkLengthOfString(arrHashtags)) {
+      evt.preventDefault();
+      formHashtags.classList.add('upload-message-error');
+    } else if (checkNumberOfHashtag(arrHashtags)) {
+      evt.preventDefault();
+      formHashtags.classList.add('upload-message-error');
+    } else if (checkSimilarHashtag(arrHashtags)) {
+      evt.preventDefault();
+      formHashtags.classList.add('upload-message-error');
+    } else if (checkForSpace(arrHashtags)) {
+      evt.preventDefault();
+      formHashtags.classList.add('upload-message-error');
+    } else {
+      formHashtags.classList.remove('upload-message-error');
+    }
+  } else {
+    formHashtags.classList.remove('upload-message-error');
+  }
+
+  if (!formDescription.value || formDescription.validity.tooShort) {
+    evt.preventDefault();
+    formDescription.classList.add('upload-message-error');
+  } else {
+    formDescription.classList.remove('upload-message-error');
+  }
+});
+
+resizeControlDec.addEventListener('click', function () {
+  if (resizeControls.value > MIN_SCALE) {
+    resizeControls.value = resizeControls.value - MIN_SCALE;
+    photo.style.transform = 'scale(' + resizeControls.value / MAX_SCALE + ')';
+  }
+});
+
+resizeControlInc.addEventListener('click', function () {
+  if (resizeControls.value < MAX_SCALE) {
+    resizeControls.value = +resizeControls.value + MIN_SCALE;
+    photo.style.transform = 'scale(' + resizeControls.value / MAX_SCALE + ')';
+  }
+});
+
+effectControls.addEventListener('click', function (evt) {
+  var target = evt.target;
+  var current = evt.currentTarget;
+
+  if (target.tagName.toLowerCase() === 'input') {
+    var effect = target.attributes['id'].nodeValue;
+
+    effect = effect.substr(INDEX_OF_STRING);
+    photo.classList.remove(photo.className);
+    photo.classList.add(effect);
   }
 });
